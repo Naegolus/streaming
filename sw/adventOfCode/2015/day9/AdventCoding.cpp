3b67570083dfe3ac5c1c069a8778f17e7e33d598
Johannes Natter <office@dsp-crowd.com>
2022-12-08 21:38:17 +0100

    AoC 2015: Add day 9

--- sw/adventOfCode/2015/day9/AdventCoding.cpp
+++ sw/adventOfCode/2015/day9/AdventCoding.cpp
@@ -34,20 +34,124 @@ void lineParse(const char *pLine)
 	if (!dists.isMember(dst))
 		dists[dst] = objectValue;
 	dists[dst][src] = dist;
+#if 0
+	Value tmp;
+
+	if (!dists.isMember("list"))
+		dists["list"] = objectValue;
+
+	tmp["src"] = src;
+	tmp["dst"] = dst;
+
+	dists["list"][to_string(dist)] = tmp;
+#endif
 }
 
-uint32_t calc()
+bool nodeFound(list<string> &visited, const string &node)
 {
-	return 0;
+	list<string>::const_iterator iter;
+
+	for (iter = visited.begin(); iter != visited.end(); ++iter)
+	{
+		if (*iter == node)
+			return true;
+	}
+
+	return false;
+}
+
+uint32_t calcShortest(
+		list<string> &visited,
+		uint8_t mode,
+		uint32_t distTraveled = 0,
+		uint32_t depth = 0)
+{
+	++depth;
+	if (depth > 12)
+	{
+		errLog(-1, "Too deep");
+		return 0;
+	}
+
+	uint32_t distNow;
+	uint32_t dist, distRes = 0;
+
+	Value::const_iterator iter;
+	string nodeNext, nodeCurr;
+	bool nextFound = false;
+
+	//infLog("Depth %d", depth);
+
+	for (iter = dists.begin(); iter != dists.end(); ++iter)
+	{
+		nodeNext = iter.key().asString();
+
+		//infLog("Trying node %s", nodeNext.c_str());
+
+		if (nodeFound(visited, nodeNext))
+			continue;
+
+		//infLog("Using node %s", nodeNext.c_str());
+
+		nextFound = true;
+
+		if (visited.size())
+		{
+			nodeCurr = visited.back();
+			distNow = dists[nodeCurr][nodeNext].asUInt();
+			//infLog("Traveling %d", distNow);
+			distTraveled += distNow;
+		}
+
+		//infLog("Traveled  %d", distTraveled);
+
+		visited.push_back(nodeNext);
+		dist = calcShortest(visited, mode, distTraveled, depth);
+		visited.pop_back();
+
+		if (visited.size())
+			distTraveled -= distNow;
+
+		if (!distRes)
+			distRes = dist;
+
+		if (mode == 0)
+		{
+			if (dist < distRes)
+				distRes = dist;
+		}
+		else
+		{
+			if (dist > distRes)
+				distRes = dist;
+		}
+	}
+
+	if (!nextFound)
+		return distTraveled;
+
+	//infLog("Depth %d: End", depth);
+
+	return distRes;
+}
+
+uint32_t calcShortest(uint8_t mode = 0)
+{
+	uint32_t res;
+	list<string> visited;
+
+	res = calcShortest(visited, mode);
+
+	return res;
 }
 
 Success AdventCoding::process()
 {
-	FILE *pFile = fopen("../input.txt", "r");
+	FILE *pFile = fopen("../input2.txt", "r");
 	//FastWriter writer;
 	StyledWriter writer;
 	string str;
-	uint32_t res1;
+	uint32_t res1, res2;
 
 	if (!pFile)
 		return procErrLog(-1, "Could not open file");
@@ -68,9 +172,11 @@ Success AdventCoding::process()
 	str = writer.write(dists);
 	cout << str;
 
-	res1 = calc();
+	res1 = calcShortest();
+	res2 = calcShortest(1);
 
 	procInfLog("Result1: %d", res1);
+	procInfLog("Result2: %d", res2);
 
 	return Positive;
 }
