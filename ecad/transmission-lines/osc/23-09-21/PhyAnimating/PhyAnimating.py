87fbb36e817c377fe224f2bb3b51504d168dfad7
Johannes Natter <office@dsp-crowd.com>
2023-10-06 10:34:47 +0200

    Fix licenses

--- ecad/transmission-lines/osc/23-09-21/PhyAnimating/PhyAnimating.py
+++ ecad/transmission-lines/osc/23-09-21/PhyAnimating/PhyAnimating.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 
+#  This file is part of the DSP-Crowd project
+#  https://www.dsp-crowd.com
+#
+#  Author(s):
+#      - Johannes Natter, office@dsp-crowd.com
+#
+#  File created on 05.10.2023
+#
+#  Copyright (C) 2023-now Authors and www.dsp-crowd.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import matplotlib as mpl
 import matplotlib.figure
 import matplotlib.backends.backend_qt5agg
