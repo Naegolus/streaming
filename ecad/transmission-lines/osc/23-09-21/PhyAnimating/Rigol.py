e3a61be9a171f4d71bfe6f12b20bd35724ba54a5
Johannes Natter <office@dsp-crowd.com>
2023-10-13 00:33:56 +0200

    Rigol.py: Remove ax2 and use abs() after fft()

--- ecad/transmission-lines/osc/23-09-21/PhyAnimating/Rigol.py
+++ ecad/transmission-lines/osc/23-09-21/PhyAnimating/Rigol.py
@@ -41,22 +41,29 @@ class DiracAnimating(PhyAnimating):
 		self.ch2 = np.array(df["CH2(V)"])
 		self.ch2 = self.ch2 - self.ch2[0]
 
-		self.axTime  = self.fig.add_subplot(3, 1, 1)
-		self.axFreq1 = self.fig.add_subplot(3, 1, 2)
-		self.axFreq2 = self.fig.add_subplot(3, 1, 3)
-		self.axFreq2twin = self.axFreq2.twiny()
+		self.axTime  = self.fig.add_subplot(2, 1, 1)
+		self.axFreq1 = self.fig.add_subplot(2, 1, 2)
+		self.axFreq1twin = self.axFreq1.twiny()
 
-		z = 10
+		z = 30
 		n = 15
 
 		pads = np.zeros([1, n * self.ch1.size])
 		self.ch1p = np.append(self.ch1, pads)
 
-		self.f1 = np.fft.fft(self.ch1)
+		self.f1 = np.abs(np.fft.fft(self.ch1))
 		self.f1z = self.f1[0 : z]
 
-		self.f1p = np.fft.fft(self.ch1p)
+		self.f1p = np.abs(np.fft.fft(self.ch1p))
 		self.f1pz = self.f1p[0 : (1 + n) * z]
+
+		self.ch2p = np.append(self.ch2, pads)
+
+		self.f2 = np.abs(np.fft.fft(self.ch2))
+		self.f2z = self.f2[0 : z]
+
+		self.f2p = np.abs(np.fft.fft(self.ch2p))
+		self.f2pz = self.f2p[0 : (1 + n) * z]
 
 	def process(self):
 
@@ -80,29 +87,21 @@ class DiracAnimating(PhyAnimating):
 
 		# ----------------------
 
+		limA = 500
+
 		self.axFreq1.clear()
 
-		self.axFreq1.plot(self.f1, label = "Freq Ch. 1", color = 'y')
+		self.axFreq1.stem(self.f1z, label = "Freq Ch. 1")
+		self.axFreq1.set_xlim(0, self.f2z.size)
+
+		self.axFreq1twin.plot(self.f1pz, label = "Freq Ch. 1", color = 'y')
+		self.axFreq1twin.plot(self.f2pz, label = "Freq Ch. 2", color = 'c')
+		self.axFreq1twin.set_xlim(0, self.f1pz.size)
+
+		self.axFreq1.set_ylim(-limA/10, limA)
 
 		self.axFreq1.set_xlabel('Frequency [?]');
 		self.axFreq1.set_ylabel('Amplitude [?]');
-
-		# ----------------------
-
-		limF = 400
-
-		self.axFreq2.clear()
-
-		self.axFreq2.stem(self.f1z, label = "Freq Ch. 1")
-		self.axFreq2.set_xlim(0, self.f1z.size)
-
-		self.axFreq2twin.plot(self.f1pz, label = "Freq Ch. 1")
-		self.axFreq2twin.set_xlim(0, self.f1pz.size)
-
-		self.axFreq2.set_ylim(-limF, limF)
-
-		self.axFreq2.set_xlabel('Frequency [?]');
-		self.axFreq2.set_ylabel('Amplitude [?]');
 
 		# ----------------------
 
